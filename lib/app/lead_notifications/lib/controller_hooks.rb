7dcd36acdccced88efa04b80e9e4f21a83dab205
mattbeedle <mattbeedle@gmail.com>
2010-08-28 22:14:21 +0200

    Version bump to 0.0.3

--- lib/app/lead_notifications/lib/controller_hooks.rb
+++ lib/app/lead_notifications/lib/controller_hooks.rb
@@ -1,8 +1,9 @@
 class LeadNotifications::ControllerHooks < Salesflip::Callback::Base
   def app_after_filter( controller, context = {} )
     return unless controller.controller_name == 'leads' && controller.action_name == 'create'
-    LeadNotifications::LeadNotificationMailer.lead_notification(
-      controller.instance_variable_get("@lead")
-    ).deliver
+    lead = controller.instance_variable_get("@lead")
+    if lead && !controller.request.format.html?
+      LeadNotifications::LeadNotificationMailer.lead_notification(lead).deliver
+    end
   end
 end
