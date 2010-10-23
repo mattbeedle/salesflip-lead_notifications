106d0ab2b56d66635cb9fda5ae65edee5bc24154
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-23 13:19:18 +0200

    Version bump to 0.0.7

--- lib/app/lead_notifications/mailers/lead_notification_mailer.rb
+++ lib/app/lead_notifications/mailers/lead_notification_mailer.rb
@@ -3,7 +3,8 @@ module LeadNotifications
 
     def lead_notification( lead )
       @lead = lead
-      mail(:to => lead.user.company.users.map(&:email), :subject => 'New Lead')
+      mail(:to => lead.user.company.users.map(&:email), :subject => 'New Lead',
+        :from => 'service@salesflip.com')
     end
   end
 end
