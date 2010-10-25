0f2cbf756b306840e5314ca2261ba02384cf6a6b
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-25 12:34:00 +0200

    Only notify benny, flo s and stephan

--- lib/app/lead_notifications/mailers/lead_notification_mailer.rb
+++ lib/app/lead_notifications/mailers/lead_notification_mailer.rb
@@ -3,7 +3,12 @@ module LeadNotifications
 
     def lead_notification( lead )
       @lead = lead
-      mail(:to => lead.user.company.users.map(&:email), :subject => 'New Lead',
+      #mail(:to => lead.user.company.users.map(&:email), :subject => 'New Lead',
+      #  :from => 'service@salesflip.com')
+      mail(:to => ['benjamin.pochhammer@1000jobboersen.de',
+        'florian.schiffer@1000jobboersen.de',
+        'stephan.mehdorn@1000jobboersen.de'],
+        :subject => 'New Lead',
         :from => 'service@salesflip.com')
     end
   end
