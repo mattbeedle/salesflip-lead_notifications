6d4c36a102bb55df7a2958f5f2d80a06ffbb5e10
Matt Beedle <matt.beedle@1000jobboersen.de>
2010-10-27 14:16:03 +0200

    Will not blow up now if lead is invalid

--- lib/app/lead_notifications/lib/controller_hooks.rb
+++ lib/app/lead_notifications/lib/controller_hooks.rb
@@ -2,7 +2,7 @@ class LeadNotifications::ControllerHooks < Salesflip::Callback::Base
   def app_after_filter( controller, context = {} )
     return unless controller.controller_name == 'leads' && controller.action_name == 'create'
     lead = controller.instance_variable_get("@lead")
-    if lead && !controller.request.format.html?
+    if lead && lead.valid? && !controller.request.format.html?
       LeadNotifications::LeadNotificationMailer.lead_notification(lead).deliver
     end
   end
